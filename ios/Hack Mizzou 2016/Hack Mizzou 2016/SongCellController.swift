f0955708aae6bd31b65acece69976e1e82de816c
Reikerseiffe <reikerseiffe@gmail.com>
2016-10-14 21:21:45 -0500

    Storyboard layout and VC
    Built out the general layout for the app and made view controllers for
    everything

---
+++ ios/Hack Mizzou 2016/Hack Mizzou 2016/SongCellController.swift
@@ -0,0 +1,24 @@
+//
+//  SongCellController.swift
+//  Hack Mizzou 2016
+//
+//  Created by Reiker Seiffe on 10/14/16.
+//  Copyright © 2016 Reiker R Seiffe. All rights reserved.
+//
+
+import UIKit
+
+class SongCellController: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
